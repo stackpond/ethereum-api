17ca3d7aa5069a17ba10837f05624274d27c4f14
stackpond <stackpond@gmail.com>
2021-01-17 11:38:00 +1100

    Updated README

---
+++ EthereumApi.Tests/TransactionsControllerTests.cs
@@ -0,0 +1,84 @@
+using EthereumApi.Core.Messaging;
+using EthereumApi.Web.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Threading;
+using System.Threading.Tasks;
+using TransactionDtoCollection = System.Collections.Generic.List<EthereumApi.Core.Dto.TransactionDto>;
+
+namespace EthereumApi.Tests
+{
+    public class TransactionsControllerTests
+    {
+        private readonly TransactionsController _transactionsController;
+        private readonly Mock<IMediator> _mediator = new Mock<IMediator>();
+
+        public TransactionsControllerTests()
+        {
+            _transactionsController = new TransactionsController(_mediator.Object);
+        }
+
+        [Test]
+        public async Task ShouldReturn200WhenGetByBlockNumberSucceeds()
+        {
+            // Arrange
+            _mediator.Setup(m => m.Send(It.IsAny<GetTransactionsByBlockNumberCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new CommandResult<TransactionDtoCollection>(new TransactionDtoCollection()));
+
+            // Act
+            var actionResult = await _transactionsController.GetByBlockNumber(1, 1) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(actionResult);
+            Assert.AreEqual(StatusCodes.Status200OK, actionResult.StatusCode.Value);
+        }
+
+        [Test]
+        public async Task ShouldReturn200WhenGetByBlockNumberFails()
+        {
+            // Arrange
+            _mediator.Setup(m => m.Send(It.IsAny<GetTransactionsByBlockNumberCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new CommandResult<TransactionDtoCollection>("Failed"));
+
+            // Act
+            var actionResult = await _transactionsController.GetByBlockNumber(1, 1) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(actionResult);
+            Assert.AreEqual(StatusCodes.Status500InternalServerError, actionResult.StatusCode.Value);
+        }
+
+        [Test]
+        public async Task ShouldReturn200WhenGetByAddressSucceeds()
+        {
+            // Arrange
+            _mediator.Setup(m => m.Send(It.IsAny<GetTransactionsByAddressCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new CommandResult<TransactionDtoCollection>(new TransactionDtoCollection()));
+
+            // Act
+            var actionResult = await _transactionsController.GetByAddress("0x22a52e0f761345ecffba06482c99dc3dea7bd8f1", 1) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(actionResult);
+            Assert.AreEqual(StatusCodes.Status200OK, actionResult.StatusCode.Value);
+        }
+
+        [Test]
+        public async Task ShouldReturn200WhenGetByAddressFails()
+        {
+            // Arrange
+            _mediator.Setup(m => m.Send(It.IsAny<GetTransactionsByAddressCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new CommandResult<TransactionDtoCollection>("Failed"));
+
+            // Act
+            var actionResult = await _transactionsController.GetByAddress("0x22a52e0f761345ecffba06482c99dc3dea7bd8f1", 1) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(actionResult);
+            Assert.AreEqual(StatusCodes.Status500InternalServerError, actionResult.StatusCode.Value);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# ethereum-api
+[![Build Status](https://travis-ci.com/stackpond/ethereum-api.svg?branch=main)](https://travis-ci.com/stackpond/ethereum-api)
+
+# Ethereum Explorer API
+
+This API internally consumes Infura API (refer https://infura.io/docs/ethereum) to get Ethereum blockchain transactions.
+
+Try out the API deployed in Heroku at https://stackpond-ethereum-api.herokuapp.com/swagger.
+
+My other project Ethereum Explorer (refer https://github.com/stackpond/ethereum-explorer) consumes this API to display Ethereum transactions.
+
+## Development setup
+
+|Software|Download Link|
+|---|---|
+|.NET Core 3.1|https://dotnet.microsoft.com/download|
+|Microsoft Visual Studio Professional 2019|https://visualstudio.microsoft.com/downloads/|
+
+
+**Commands below can be executed in the command line mode if Visual Studio IDE is not used**
+
+### Build
+
+Run `dotnet build` to build.
+
+### Running
+
+|OS|Command|
+|---|---|
+|Windows|`dotnet run --project .\EthereumApi.Web\EthereumApi.Web.csproj`|
+|Non-windows|`dotnet run --project ./EthereumApi.Web/EthereumApi.Web.csproj`|
+
+### Test
+
+Run `dotnet test` to test.
+
+## Important notes
+
+The transaction results are paged for better user experience.
+
+Infura API has rate limit imposed (refer https://infura.io/pricing) which may also slow down the processing.
+
+The search by address is especially slow because Infura API does not provide any API for this use. Hence every transactions of every block needs to be processed and it can a very long time before the results are fetched. I have few ideas to speed this up by creating an indexer service separate from core API which can then be consumed for faster processing. This is TBD.
